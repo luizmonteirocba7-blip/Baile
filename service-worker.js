b0a584db71d30196be4e3a02a0e2c3a4c7b5aa67
luizmonteirocba7-blip <luiz.monteirocba7@gmail.com>
2026-05-30 08:31:41 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
+const CACHE = 'baile-v1';
+const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request));
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    if (e.request.method !== 'GET') return;
+    e.respondWith(
+        caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
 });
