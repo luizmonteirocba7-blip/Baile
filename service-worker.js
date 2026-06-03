9d7ad2910714eea1d793d5d72d745efa722045b4
luizmonteirocba7-blip <luiz.monteirocba7@gmail.com>
2026-06-03 16:17:58 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'baile-v1';
+const CACHE = 'baile-v2';
 const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
