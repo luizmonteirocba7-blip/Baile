f3ee49bf9f5e07b0f7ec2f27f82aff4e17ffef73
luizmonteirocba7-blip <luiz.monteirocba7@gmail.com>
2026-06-09 19:50:37 -0400

    Update cache version from v6 to v7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'baile-v6';
+const CACHE = 'baile-v7';
 const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
